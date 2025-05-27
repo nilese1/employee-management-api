a17b485520d3a91f0824de14952e617ee9940146
Ethan Niles <notethansemail123456789@gmail.com>
2025-05-27 11:49:25 -0400

    Added filtering for employee view set (not renderable)

--- employee_management_api/employees/views.py
+++ employee_management_api/employees/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import EmployeeSerializer, DepartmentSerializer, PerformanceSerializer
 from .models import Employee, Department, Performance
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    # filter by department and date joined
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["department", "date_of_joining"]
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
